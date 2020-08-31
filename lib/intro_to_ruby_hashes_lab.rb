3bf81716fd0ae71107ec60b0ce331cabcdd7e946
Jaquan1314 <javaughngrant@gmail.com>
2020-08-30 20:54:52 -0400

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,27 @@
 def new_hash
   # return an empty hash
+  empty_hash = {}
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  valid_hash = {
+    name: "Javaughn Grant",
+    age: 21
+  }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  hashy = {
+    name: "Grace Hopper"
+  }
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  another_hashy = {
+    id: number
+  }
 end
